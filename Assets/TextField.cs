fba7ae5aae101e0b3da7ad7036695534adba8cfb
13212312312 <nejneriumihai@gmail.com>
2023-06-21 02:14:44 +0300

    Am facut server

---
+++ Assets/TextField.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextField : MonoBehaviour
+{
+    public string stringToEdit = "Hello World";
+
+    void OnGUI()
+    {
+        // Make a text field that modifies stringToEdit.
+        stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+    }
+}
